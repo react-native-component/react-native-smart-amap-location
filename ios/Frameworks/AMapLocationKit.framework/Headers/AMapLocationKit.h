4b79e10d22ef06c8d2e3fbb4ff3a02adad2d1d6b
Aevit <Aevitx@gmail.com>
2017-06-19 11:48:55 +0800

    update amap sdk (without JSPatch)

--- ios/Frameworks/AMapLocationKit.framework/Headers/AMapLocationKit.h
+++ ios/Frameworks/AMapLocationKit.framework/Headers/AMapLocationKit.h
@@ -3,7 +3,7 @@
 //  AMapLocationKit
 //
 //  Created by AutoNavi on 15/10/22.
-//  Copyright © 2015年 AutoNavi. All rights reserved.
+//  Copyright © 2015年 Amap. All rights reserved.
 //
 
 #import <AMapLocationKit/AMapLocationVersion.h>
@@ -11,3 +11,7 @@
 #import <AMapLocationKit/AMapLocationManager.h>
 #import <AMapLocationKit/AMapLocationCommonObj.h>
 #import <AMapLocationKit/AMapLocationRegionObj.h>
+
+#import <AMapLocationKit/AMapGeoFenceRegionObj.h>
+#import <AMapLocationKit/AMapGeoFenceManager.h>
+#import <AMapLocationKit/AMapGeoFenceError.h>
